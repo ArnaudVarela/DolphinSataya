508380443d1280db326c5a1590abeb06a1d0221b
ArnaudVarela <arnaud.varela@gmail.com>
2025-10-21 22:53:40 +0200

    Create home.ts

---
+++ app/data/home.ts
@@ -0,0 +1,48 @@
+// app/data/home.ts
+export const WHATSAPP = "https://wa.me/20XXXXXXXXXX?text=Bonjour%20je%20souhaite%20des%20infos%20sur%20la%20croisi%C3%A8re%20Sataya";
+export const PAYMENT_LINK = "https://buy.stripe.com/test_xxxxxxxxxxxxx";
+
+export const trust = [
+  "+1\u00a0000 nageurs accompagn\u00e9s",
+  "Note 4,8/5",
+  "Encadrement dipl\u00f4m\u00e9",
+  "Bateau cat\u00e9gorie confort",
+];
+
+export const usps = [
+  { title: "Snorkeling quotidien", text: "2\u20133 mises \u00e0 l\u2019eau guid\u00e9es selon conditions." },
+  { title: "Bateau confortable", text: "Cabines doubles, pont sup\u00e9rieur pour pilates." },
+  { title: "Accompagnement bienveillant", text: "Briefings, s\u00e9curit\u00e9, p\u00e9dagogie." },
+  { title: "Ethique animale", text: "Distances, temps limit\u00e9, z\u00e9ro nourriture." },
+  { title: "Photos souvenirs", text: "Prises depuis l\u2019eau (selon disponibilit\u00e9)." },
+  { title: "Transferts organis\u00e9s", text: "Depuis l\u2019a\u00e9roport de Marsa Alam." },
+];
+
+export const itinerary = [
+  { day: "J1", title: "Arriv\u00e9e & embarquement", text: "Installation, briefing s\u00e9curit\u00e9." },
+  { day: "J2", title: "Premi\u00e8re rencontre", text: "Mise \u00e0 l\u2019eau douce & observation." },
+  { day: "J3", title: "Rythme croisi\u00e8re", text: "Sessions selon m\u00e9t\u00e9o & faune." },
+  { day: "J4", title: "Pilates & respiration", text: "Mobilit\u00e9, souffle, r\u00e9cup\u00e9ration." },
+  { day: "J5", title: "Lagon & r\u00e9cif", text: "Snorkeling color\u00e9 (selon conditions)." },
+  { day: "J6", title: "Pleine pr\u00e9sence", text: "Respect de la charte dauphins." },
+  { day: "J7", title: "Derni\u00e8res sessions", text: "Temps d\u2019eau adapt\u00e9 au groupe." },
+  { day: "J8", title: "D\u00e9barquement", text: "Retour & transferts." },
+];
+
+export const dates = [
+  { when: "12\u201319 avril", cabin: "Cabine double", price: "1\u00a0XXX \u20ac", left: 4, link: PAYMENT_LINK },
+  { when: "3\u201310 mai", cabin: "Cabine double", price: "1\u00a0XXX \u20ac", left: 6, link: PAYMENT_LINK },
+  { when: "17\u201324 mai", cabin: "Cabine double", price: "1\u00a0XXX \u20ac", left: 3, link: PAYMENT_LINK },
+];
+
+export const reviews = [
+  { name: "Camille", text: "Moments magiques, \u00e9quipe aux petits soins. On se sent en s\u00e9curit\u00e9 du d\u00e9but \u00e0 la fin." },
+  { name: "Romain", text: "Approche respectueuse des dauphins, briefings clairs, bateau top." },
+  { name: "Sarah", text: "J\u2019ai ador\u00e9 l\u2019alternance snorkeling / pilates. Je repars ressourc\u00e9e." },
+];
+
+export const blog = [
+  { title: "Pr\u00e9parer son masque & tuba", intro: "La checklist simple.", href: "#" },
+  { title: "Comprendre le comportement des dauphins", intro: "Observer sans d\u00e9ranger.", href: "#" },
+  { title: "Respiration & apn\u00e9e douce", intro: "Les bases pour d\u00e9buter.", href: "#" },
+];
