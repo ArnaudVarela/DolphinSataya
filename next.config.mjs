b4252fb41f28b985afa4e8a6ba5de8aca22e47b4
ArnaudVarela <75020810+ArnaudVarela@users.noreply.github.com>
2025-10-21 21:11:26 +0200

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,5 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers(){ return [{ source: "/:path*.(mp4|webm)", headers: [{ key:"Cache-Control", value:"public, max-age=2592000, immutable"}]}]; }
+};
+export default nextConfig;
