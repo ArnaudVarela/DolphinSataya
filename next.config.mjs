9656c9e4d5562d5f23c5c89f06ff345cded35d42
ArnaudVarela <75020810+ArnaudVarela@users.noreply.github.com>
2025-10-21 22:15:58 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers(){ return [{ source: "/:path*.(mp4|webm)", headers: [{ key:"Cache-Control", value:"public, max-age=2592000, immutable"}]}]; }
   eslint: { ignoreDuringBuilds: true },
+  async headers() {
+    return [
+      {
+        source: "/:path*.(mp4|webm)",
+        headers: [{ key: "Cache-Control", value: "public, max-age=2592000, immutable" }],
+      },
+    ];
+  },
 };
 export default nextConfig;
-
-
